b5f582c95452edeee1081fba886cbadc28ff69f9
Jonnyzhang0122 <ruotianzhang0122@gmail.com>
2016-12-08 16:55:21 -0500

    All features included

---
+++ MayShow_project/app/src/main/java/com/beginners/myapplication/CameraActivity.java
@@ -0,0 +1,106 @@
+package com.beginners.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.kickflip.sdk.Kickflip;
+import io.kickflip.sdk.Share;
+import io.kickflip.sdk.activity.ImmersiveActivity;
+import io.kickflip.sdk.api.json.Stream;
+import io.kickflip.sdk.av.AVRecorder;
+import io.kickflip.sdk.av.BroadcastListener;
+import io.kickflip.sdk.av.Broadcaster;
+import io.kickflip.sdk.av.FullFrameRect;
+import io.kickflip.sdk.av.SessionConfig;
+import io.kickflip.sdk.exception.KickflipException;
+import io.kickflip.sdk.fragment.BroadcastFragment;
+import io.kickflip.sdk.view.GLCameraEncoderView;
+
+/**
+ * Demonstrates using the Kickflip SDK components to
+ * create a more traditional Camera app that allows creating multiple
+ * recordings per Activity lifecycle.
+ *
+ * In this example recording will stop if the Activity proceeds through {@link #onStop()}
+ *
+ * <b>Note:</b> This Activity is marked in AndroidManifest.xml with the property android:configChanges="orientation|screenSize"
+ *              This is a shortcut to prevent the onDestroy ... onCreate ... onDestroy cycle when the screen powers off. Without this
+ *              shortcut, more careful management of application state is required to make sure you don't create a recorder and set the preview
+ *              display as a result of that ephemeral onCreate. If you do, you'll get a crash as the Camera won't be able to be acquired.
+ *              See: http://stackoverflow.com/questions/10498636/prevent-android-activity-from-being-recreated-on-turning-screen-off
+ */
+public class CameraActivity extends ImmersiveActivity implements BroadcastListener {
+    private static final String TAG = "CameraActivity";
+
+    private CameraFragment mFragment;
+    private BroadcastListener mMainBroadcastListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        mMainBroadcastListener = Kickflip.getBroadcastListener();
+        Kickflip.setBroadcastListener(mMainBroadcastListener);
+
+        if (savedInstanceState == null) {
+            mFragment = CameraFragment.getInstance();
+            getFragmentManager().beginTransaction()
+                    .replace(io.kickflip.sdk.R.id.container, mFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mFragment != null) {
+            mFragment.stopBroadcasting();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onBroadcastStart() {
+        mMainBroadcastListener.onBroadcastStart();
+    }
+
+    @Override
+    public void onBroadcastLive(Stream stream) {
+        mMainBroadcastListener.onBroadcastLive(stream);
+    }
+
+    @Override
+    public void onBroadcastStop() {
+        finish();
+        mMainBroadcastListener.onBroadcastStop();
+    }
+
+    @Override
+    public void onBroadcastError(KickflipException error) {
+        mMainBroadcastListener.onBroadcastError(error);
+    }
+
+}
---
+++ MayShow_project/app/src/main/java/com/beginners/myapplication/Myprofile.java
@@ -0,0 +1,337 @@
+package com.beginners.myapplication;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import io.kickflip.sdk.Kickflip;
+import io.kickflip.sdk.Share;
+import io.kickflip.sdk.api.KickflipApiClient;
+import io.kickflip.sdk.api.KickflipCallback;
+import io.kickflip.sdk.api.json.Response;
+import io.kickflip.sdk.api.json.Stream;
+import io.kickflip.sdk.api.json.StreamList;
+import io.kickflip.sdk.api.json.User;
+import io.kickflip.sdk.exception.KickflipException;
+
+import static com.beginners.myapplication.SigninActivity.mKickflip;
+
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Large screen devices (such as tablets) are supported by replacing the ListView
+ * with a GridView.
+ * <p/>
+ */
+public class Myprofile extends Fragment implements AbsListView.OnItemClickListener, AbsListView.OnItemLongClickListener, SwipeRefreshLayout.OnRefreshListener {
+    public static final String TAG = "StreamListFragment";
+    private static final String SERIALIZED_FILESTORE_NAME = "streams";
+    private static final boolean VERBOSE = true;
+
+    private StreamListFragmenListener mListener;
+    private SwipeRefreshLayout mSwipeLayout;
+
+    private List<Stream> mStreams;
+    private boolean mRefreshing;
+
+    private int mCurrentPage = 1;
+    private static final int ITEMS_PER_PAGE = 10;
+
+    private DatabaseReference upload;
+
+    private TextView musername;
+
+    /**
+     * The fragment's ListView/GridView.
+     */
+    private AbsListView mListView;
+
+    /**
+     * The Adapter which will be used to populate the ListView/GridView with
+     * Views.
+     */
+    private StreamAdapter mAdapter;
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+        final Stream stream = mAdapter.getItem(position);
+        KickflipCallback cb = new KickflipCallback() {
+            @Override
+            public void onSuccess(Response response) {
+                if (getActivity() != null) {
+                    if (mKickflip.activeUserOwnsStream(stream)) {
+                        mAdapter.remove(stream);
+                        mAdapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getActivity(), getActivity().getString(R.string.stream_flagged), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(KickflipException error) {}
+        };
+
+        if (mKickflip.activeUserOwnsStream(stream)) {
+            stream.setDeleted(true);
+            mKickflip.setStreamInfo(stream, cb);
+        } else {
+            mKickflip.flagStream(stream, cb);
+        }
+        return false;
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Stream stream = mAdapter.getItem(position);
+        mListener.onStreamPlaybackRequested(stream.getStreamUrl());
+    }
+
+
+
+
+    private EndlessScrollListener mEndlessScrollListener = new EndlessScrollListener() {
+        @Override
+        public void onLoadMore(int page, int totalItemsCount) {
+            Log.i(TAG, "Loading more streams");
+            getStreams(false);
+        }
+    };
+
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public Myprofile() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        loadPersistedStreams();
+        getStreams(true);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        persistStreams();
+    }
+
+
+    /**
+     * Load persisted Streams from disk if available.
+     */
+    private void loadPersistedStreams() {
+        if (getActivity() != null) {
+            Object streams = LocalPersistence.readObjectFromFile(getActivity(), SERIALIZED_FILESTORE_NAME);
+            if (streams != null) {
+                displayStreams((List<Stream>) streams, false);
+            }
+        }
+    }
+
+    /**
+     * Serialize a few Streams to disk so the UI is quickly populated
+     * on application re-launch
+     *
+     * If we had reason to keep a robust local copy of the data, we'd use sqlite
+     */
+    private void persistStreams() {
+        if (getActivity() != null) {
+            while (mStreams.size() > 7) {
+                mStreams.remove(mStreams.size()-1);
+            }
+            LocalPersistence.writeObjectToFile(getActivity(), mStreams, SERIALIZED_FILESTORE_NAME);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Set the adapter
+        mListView = (AbsListView) view.findViewById(android.R.id.list);
+        mListView.setOnScrollListener(mEndlessScrollListener);
+        mListView.setEmptyView(view.findViewById(android.R.id.empty));
+        // Why does this selection remain if I long press, release
+        // without activating onListItemClick?
+        //mListView.setSelector(R.drawable.stream_list_selector_overlay);
+        //mListView.setDrawSelectorOnTop(true);
+
+        musername = (TextView) view.findViewById(R.id.username);
+
+        mSwipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.refreshLayout);
+        mSwipeLayout.setOnRefreshListener(this);
+
+
+        // Set OnItemClickListener so we can be notified on item clicks
+        mListView.setOnItemClickListener(this);
+        mListView.setOnItemLongClickListener(this);
+        setupListViewAdapter();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        /*try {
+            //mListener = (StreamListFragmenListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement StreamListFragmenListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+
+    /**
+     * Fetch Streams and display in ListView
+     *
+     * @param refresh whether this fetch is for a subsequent page
+     *                      or to refresh the first page
+     */
+    private void getStreams(final boolean refresh) {
+        if (mKickflip.getActiveUser() == null || mRefreshing) return;
+        mRefreshing = true;
+        if (refresh) mCurrentPage = 1;
+        //Toast.makeText(getActivity(), "wahaha "+ mKickflip.getActiveUser().getName(),Toast.LENGTH_SHORT).show();
+
+        musername.setText(mKickflip.getActiveUser().getName());
+
+
+        mKickflip.getStreamsByUsername(mKickflip.getActiveUser().getName(), mCurrentPage, ITEMS_PER_PAGE, new KickflipCallback() {
+
+            //mKickflip.getStreamsByUsername(mKickflip.getActiveUser().getName(), mCurrentPage, ITEMS_PER_PAGE, new KickflipCallback() {
+            @Override
+            public void onSuccess(Response response) {
+                if (VERBOSE) Log.i("API", "request succeeded " + response);
+                if (getActivity() != null) {
+                    displayStreams(((StreamList) response).getStreams(), !refresh);
+                }
+                Toast.makeText(getActivity(), mKickflip.getActiveUser().getName(),Toast.LENGTH_SHORT).show();
+                mSwipeLayout.setRefreshing(false);
+                mRefreshing = false;
+                mCurrentPage++;
+            }
+
+            @Override
+            public void onError(KickflipException error) {
+                if (VERBOSE) Log.i("API", "request failed " + error.getMessage());
+                if (getActivity() != null) {
+                    showNetworkError();
+                }
+                //Toast.makeText(getActivity(),  mKickflip.getActiveUser().getName(),Toast.LENGTH_SHORT).show();
+                mSwipeLayout.setRefreshing(false);
+                mRefreshing = false;
+            }
+        });
+    }
+
+    private void setupListViewAdapter() {
+        if (mAdapter == null) {
+            mStreams = new ArrayList<>(0);
+            mAdapter = new StreamAdapter(getActivity(), mStreams);
+            mAdapter.setNotifyOnChange(false);
+            mListView.setAdapter(mAdapter);
+            if (mKickflip.getActiveUser() != null) {
+                mAdapter.setUserName(mKickflip.getActiveUser().getName());
+            }
+        }
+    }
+
+    /**
+     * Display the given List of {@link io.kickflip.sdk.api.json.Stream} Objects
+     *
+     * @param streams a List of {@link io.kickflip.sdk.api.json.Stream} Objects
+     * @param append whether to append the given streams to the current list
+     *               or use the given streams as the absolute dataset.
+     */
+    private void displayStreams(List<Stream> streams, boolean append) {
+        if (append) {
+            mStreams.addAll(streams);
+        } else {
+            mStreams = streams;
+        }
+        Collections.sort(mStreams);
+        mAdapter.refresh(mListView, mStreams);
+        if (mStreams.size() == 0) {
+            showNoBroadcasts();
+        }
+    }
+
+    /**
+     * Inform the user that a network error has occured
+     */
+    public void showNetworkError() {
+        setEmptyListViewText(getString(R.string.no_network));
+    }
+
+    /**
+     * Inform the user that no broadcasts were found
+     */
+    public void showNoBroadcasts() {
+        setEmptyListViewText(getString(R.string.no_broadcasts));
+    }
+
+    /**
+     * If the ListView is hidden, show the
+     *
+     * @param text
+     */
+    private void setEmptyListViewText(String text) {
+        View emptyView = mListView.getEmptyView();
+
+        if (emptyView instanceof TextView) {
+            ((TextView) emptyView).setText(text);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!mRefreshing) {
+            getStreams(true);
+        }
+
+    }
+
+
+    public interface StreamListFragmenListener {
+        public void onStreamPlaybackRequested(String url);
+    }
+
+}

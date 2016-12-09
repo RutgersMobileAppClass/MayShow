5a9a2ec3d9d16674a9606a28fc7caf0c8878b982
XueweiLi4108 <xuewei.li@rutgers.edu>
2016-12-08 23:00:38 -0500

    update

--- Application test 3/app/src/main/java/com/beginners/myapplication/MainFragmentInteractionListener.java
+++
@@ -1,5 +0,0 @@
-package com.beginners.myapplication;
-//All activities used with MainActivity implement this interface for communication.
-public interface MainFragmentInteractionListener {
-    public void onFragmentEvent(MainFragment.EVENT event);
-}

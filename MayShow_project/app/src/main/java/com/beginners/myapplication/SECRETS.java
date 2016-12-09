523cdacaf637d90d1931f44349be2e95d2eb1a0c
Jonnyzhang0122 <ruotianzhang0122@gmail.com>
2016-12-08 22:28:13 -0500

    Final

--- MayShow_project/app/src/main/java/com/beginners/myapplication/SECRETS.java
+++
@@ -1,5 +0,0 @@
-package com.beginners.myapplication;
-        public class SECRETS {
-            public static final String CLIENT_KEY = "J?BMo?6-3Z.@mxMz8w4m7OaQCqxJouydKkXhP0tB";
-            public static final String CLIENT_SECRET = "0wA3wkUbbf6Hq0Nc-lJLpZxLx_PXh.AJlc-Lyd@PJiW55yJyHrEg=;Ov6be;ffBGXYQpA48S1tdIBVZgS4Qe@Uz9DTYL-IF!Z!vspqh3f?KU6WjkyE7d9eTlISSE_pQ5";
-        }
